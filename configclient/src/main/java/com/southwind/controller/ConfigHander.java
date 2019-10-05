6338a626feb19d8cc0b8706fb8522deda44cfeb5
贺彪 <46711556+hexiaobiao@users.noreply.github.com>
2019-10-05 22:51:34 +0800

    Add files via upload

---
+++ configclient/src/main/java/com/southwind/controller/ConfigHander.java
@@ -0,0 +1,15 @@
+package com.southwind.controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/hello")
+class HelloHandler {
+    @Value("${server.port}")
+    private String port;
+    @GetMapping("/index")
+    public String index(){
+        return this.port;
+    }
+}
